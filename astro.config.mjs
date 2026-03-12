e5d770cea3eb5240d0877dc22ce917930f97e497
Kristian Duda <kristian.duda@extropy.sk>
2026-03-12 10:11:29 +0100

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jk.extropy.sk',
   vite: {
     plugins: [tailwindcss()],
   },
